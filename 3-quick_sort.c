c8f0825c3d622b66671a7cc38cbe08e214e287a2
wdmd2022 <doug@dougrules.com>
2022-08-17 18:43:31 -0700

    upload 3 edits

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,10 +1,10 @@
 #include "sort.h"
 
 /**
- * quick_sort - sorts quickly
- *
+ * quick_sort - sort an array of integers using Lomuto-style quick sort
  * @array: array to be sorted
  * @size: size of the array
+ * Return: nothing at all
  */
 
 void quick_sort(int *array, size_t size)
